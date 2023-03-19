2c800a58f2cb5ed6ef64f6f75c605aac940fe5d6
Nitesh Kumar <kumarnitesh88441@gmail.com>
2023-03-19 21:08:01 +0530

    middaleware

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -2,17 +2,42 @@ const Movie=require('../models/movie.model');
 
 exports.getAllMovies=async(req,res)=>{
     let objQury={};
-
     if(req.query.name!=undefined){
         objQury.name=req.query.name;
     }
     console.log(objQury)
     try{
-        const movies=Movie.find(objQury);
+        const movies=await Movie.find(objQury);
         console.log(objQury)
         res.status(200).send(movies);
     }catch(err){
+        res.status(500).send({
+            message:err.message
+        })
         console.log(err);
     }
 
 }
+
+exports.getMovieById=async (req,res)=>{
+
+    try{
+        let movie=await Movie.find({
+            _id:req.params.id
+        })
+        res.status(200).send({
+            message:"fetch data successfull",
+            success:true,
+            data:movie
+        })
+    }catch(err){
+        res.status(200).send({
+            message:"fetch data failed",
+            success:false,
+            data:{}
+        })
+    }
+}
+
+
+
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,5 +1,6 @@
 const MovieController=require('../controllers/movie.controller');
 
-module.exports=function (app){
+module.exports=function(app){
     app.get("/mba/api/movies",MovieController.getAllMovies);
+    app.get("/mba/api/movie/:id",MovieController.getMovieById);
 }
