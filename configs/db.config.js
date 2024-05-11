54b94212e5ffd189c18f50ffe1abe3076e2ee223
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2024-05-12 01:07:02 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 
 module.exports={
     DB_NAME:"crm_db",
-    DB_URL:"mongodb://127.0.0.1:27017/Movie_Booking"
+    DB_URL:"mongodb+srv://developer:nitesh-developer@cluster0.monh9xn.mongodb.net/Movie-booking?retryWrites=true&w=majority&appName=Cluster0"
 }
